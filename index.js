9e647e45a70dcc94f13c7d9af3be5046d68c52f2
Frank Qian <109840571+jiachen88@users.noreply.github.com>
2022-11-20 17:57:02 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,38 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const fs = require('fs')
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
+function LoadVideoData() {
+    const videoData = fs.readFileSync('./data/video-details.json', 'utf8');
+    return JSON.parse(videoData);
+};
+
+function UploadNewVideo(newVideo) {
+    const videos = LoadVideoData();
+    videos.push(newVideo);
+    fs.writeFileSync('./data/video-details.json', JSON.stringify(videos));
+    return 'Video Uploaded Successfully'
+};
 
 
 app.get('/', function (req, res) {
     res.send('Hello World')
-})
+});
+
+app.get('/videos', function (req, res) {
+    res.json(LoadVideoData());
+});
+
+app.get('/videos/:videoId', function (req, res) {
+    const foundVideo = videos.find((videos) => {
+        return videos.id === req.params.id;
+    });
+    res.status(200).json(foundVideo);
+});
+
 
 app.listen(5000, function () {
     console.log('server running at http://localhost:' + 5000)
-})
+});
